3c1664f8dd509a1fde3ec27fd863bc4534aaad73
Jack Robison <jackrobison@lbry.io>
2022-03-27 14:17:04 -0400

    manylinux_2_24_x86_64

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,1 @@
-docker run --rm -v $(pwd):/lbry-rocksdb quay.io/pypa/manylinux2014_x86_64 /bin/bash lbry-rocksdb/scripts/make-wheels.sh
+docker run --rm -v $(pwd):/lbry-rocksdb quay.io/pypa/manylinux_2_24_x86_64 /bin/bash lbry-rocksdb/scripts/make-wheels.sh
