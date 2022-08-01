2b98b5ce8af15702d61b5e0b802ad0853dee9564
Jonathan Moody <103143855+moodyjon@users.noreply.github.com>
2022-08-01 17:58:58 -0400

    Update to 6.29.4 plus 752fea5d4400198bc8a591b7d795d1c8d5f12230

--- rocksdb/__init__.py
+++ rocksdb/__init__.py
@@ -1,4 +1,4 @@
 from ._rocksdb import *
 
-ROCKSDB_VERSION = '6.25.3'  # 0103296f39ec3fd89b4cdda9687c63fde90eec24
+ROCKSDB_VERSION = '6.29.4'  # ad155621274f6bb459fe390c7be268f2a22191a6
 __version__ = "0.8.2"
